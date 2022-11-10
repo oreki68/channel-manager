7fb92e081ceb7eef70b8dd89ce3fa47177f2e367
oreki68 <82082866+oreki68@users.noreply.github.com>
2022-11-10 15:51:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,8 @@ from telethon.sessions import StringSession
 api_id = int(os.environ.get('API_ID'))
 api_hash = os.environ.get('API_HASH')
 string_session = os.environ.get('STRING_SESSION')
+bot_token = os.environ.get('BOT_TOKEN')
+
+bot = TelegramClient('bot',api_id,api_hash).start(bot_token=bot_token)
 
 client = TelegramClient(StringSession(string_session), api_id, api_hash)
